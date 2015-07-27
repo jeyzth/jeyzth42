454d72562817f39df90e147b540eea817b6172ab
jeyzth <jeyzth@gmail.com>
2015-07-27 23:46:27 +0300

    ticket:1 for #1 (https://trac.42cc.co/agiloprojects/42-test-jeyzth4/ticket/1) Change test model

--- jeyzth42/tests/models/test_auth.py
+++ jeyzth42/tests/models/test_auth.py
@@ -52,3 +52,27 @@ class TestPermission(ModelTest):
         permission_name="test_permission",
         description="This is a test Description"
     )
+
+class TestTaskOne:
+   """Unit test case for the ``TaskOne`` model."""
+   
+   klass = model.TaskOne
+   attrs = dict(
+       name="Tester",
+       lastname="SurTester",
+       dateofbird="28.07.2015"
+       bio=""" What does field may be consist?
+       My biografy? It take many words.
+       Biological kind? Homo sapiens.
+       Other variants&
+       """
+       email="tester.taskone@yahoo.com"
+       skype="tester.taskone"
+       jabber="tester@xmpp.jp"
+       othr="""
+       phone + 1 333 12345678
+       mobile +1 909 65432110
+       fax +1 929 67189016
+       """
+   )
+   
